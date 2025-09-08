06f246f93c9e2a85f6c69dcedf0c5f193668bb0d
Marlon <marlonyesid33@gmail.com>
2025-09-07 21:23:03 -0500

    Finalizacion de la clase ordenamiento y consolidacion modelo

--- Modelo/Ordenamiento.java
+++ Modelo/Ordenamiento.java
@@ -2,45 +2,54 @@ package Modelo;
 
 public class Ordenamiento {
 
+    private int intercambios;
+    private int comparaciones;
+    private long tiempo;
     
-    // Burbuja distancia Marchas
+
+
+        // Burbuja distancia Marchas
         public Candidato[] bubbleSort(Candidato[] candidatos) {
-        int intercambios = 0;
         long tiempoInicio = System.nanoTime();
-        
         for (int i = 0; i < candidatos.length - 1; i++) {
             for (int j = 0; j < candidatos.length - 1 - i; j++) {
-                // Si el siguiente tiene más clases perdidas, intercambiamos
+                this.comparaciones++;
                 if (candidatos[j].getDistanciaMarchas() > candidatos[j + 1].getClasesPerdidas()) {
                     Candidato temp = candidatos[j];
                     candidatos[j] = candidatos[j + 1];
                     candidatos[j + 1] = temp;
-                    intercambios++;
+                    this.intercambios++;
                 }
             }
         }
         long tiempoFin = System.nanoTime();
-
+        this.tiempo = tiempoFin-tiempoInicio;
         return candidatos;
     }
 
     // Insercion clasesPeridas
     public Candidato[] insetionSort(Candidato[] candidatos){
-        int intercambios = 0;
         long tiempoInicio = System.nanoTime();
         
         for(int i=1; i<candidatos.length; i ++){
             int a = candidatos[i].getClasesPerdidas();
             int j = i-1;
             
-            while (j>=0 && candidatos[j].getClasesPerdidas()>a){
-                candidatos[j+1].setClasesPerdidas(candidatos[j].getClasesPerdidas());
-                j--;
-                intercambios++;
+            while (j>=0){
+                this.comparaciones++;
+                if(candidatos[j].getClasesPerdidas()>a){
+                    candidatos[j+1].setClasesPerdidas(candidatos[j].getClasesPerdidas());
+                    j--;
+                    this.intercambios++;
+                }
+                else
+                break;
             }
             candidatos[j+1].setClasesPerdidas(a);
+            this.intercambios++;
         }
         long tiempoFin = System.nanoTime();
+        this.tiempo = tiempoFin-tiempoInicio;
         
         return candidatos;
         
@@ -48,7 +57,15 @@ public class Ordenamiento {
 
     // QuickSort ValorCorrupocion
     public Candidato[] quickSort(Candidato[]candidatos, int min, int max){
-        //joder claro que marlon es un bot
+        long tiempoInicio = System.nanoTime();
+        if(min<max){
+            int pivote = particion(candidatos, min, max);
+            quickSort(candidatos, min, pivote-1);
+            quickSort(candidatos, pivote+1, max);
+        }
+        long tiempoFin = System.nanoTime();
+        this.tiempo = tiempoFin-tiempoInicio;
+        return candidatos;
     }
 
     public int particion(Candidato[] candidatos, int min, int max){
@@ -56,11 +73,13 @@ public class Ordenamiento {
         int i = min-1;
         
         for(int j = min; j<max; j++){
+            this.comparaciones++;
             if (candidatos[j].getValorCorrupcion()<pivote) {
                 i++;
                 int a = candidatos[i].getValorCorrupcion();
                 candidatos[i].setValorCorrupcion(candidatos[j].getClasesPerdidas());
                 candidatos[j].setValorCorrupcion(a);
+                this.intercambios++;
             }
         }
         int b = candidatos[i].getValorCorrupcion();
@@ -71,16 +90,16 @@ public class Ordenamiento {
     }
 
     // MergeSort valorPrbendas
-
-    public Candidato[] mergeSort(Candidato[] candidatos){
-        
+    public Candidato[] mergeSort(Candidato[]candidatos){
         long tiempoInicio = System.nanoTime();
-        Candidato[] resultados = mergeSortRec(candidatos);
+        Candidato[] resultado = mergeSortRec(candidatos);
         long tiempoFin = System.nanoTime();
-        return resultados;
+        this.tiempo = tiempoFin-tiempoInicio;
+        return resultado;
     }
 
     public Candidato[] mergeSortRec(Candidato[] candidatos){
+        
         if (candidatos.length <=1){
                 return candidatos;
             }
@@ -89,58 +108,94 @@ public class Ordenamiento {
             
             
             Candidato[] izq = new Candidato[mitad];
-            Candidato[] der = new Candidato[Candidato.length - mitad];
+            Candidato[] der = new Candidato[candidatos.length - mitad];
             
             System.arraycopy(candidatos, 0, izq, 0, mitad);
-            System.arraycopy(candidatos, mitad, der, 0, candidatos.length-1);
+            System.arraycopy(candidatos, mitad, der, 0, candidatos.length-mitad);
+
+            Candidato[] izqOrdenada = mergeSortRec(izq);
+            Candidato[] derOrdenada = mergeSortRec(der);
             
-            return merge(izq, der);
+            return merge(izqOrdenada, derOrdenada);
     }
 
-        public Candidato[] merge(Candidato[] izq, Candidato[] der ){
-            Candidato[] resultado = new Candidato[izq.length + der.length];
-            int i = 0, j = 0, k = 0;
+    public Candidato[] merge(Candidato[] izq, Candidato[] der ){
+        Candidato[] resultado = new Candidato[izq.length + der.length];
+        int i = 0, j = 0, k = 0;
 
-            while (i < izq.length && j < der.length) {
-                if (izq[i].getValorPrebendas()>= der[j].getValorPrebendas()) {
-                    resultado[k++] = izq[i++];
-            } else {
+        while (i < izq.length && j < der.length) {
+            this.comparaciones++;
+            if (izq[i].getValorPrebendas()>= der[j].getValorPrebendas()) {
+                resultado[k++] = izq[i++];
+                this.intercambios++;
+            } 
+            else {
             resultado[k++] = der[j++];
+            this.intercambios++;
+            }
         }
-    }
 
-    while (i < izq.length) {
-        resultado[k++] = izq[i++];
-    }
-
-    while (j < der.length) {
-        resultado[k++] = der[j++];
-    }
-
-    return resultado;
+        while (i < izq.length) {
+            resultado[k++] = izq[i++];
+            this.intercambios++;
         }
+
+        while (j < der.length) {
+            resultado[k++] = der[j++];
+            this.intercambios++;
+        }
+
+        return resultado;
+    }
     // SelectionSort  numSobornos
 
     public Candidato[] seleccionSort(Candidato[] candidatos){
-        int intercambios = 0;
         long tiempoInicio = System.nanoTime();
         
             for (int i = 0; i < candidatos.length; i++) {
-                // hacemos el recorrido para las comparaciones
+                this.comparaciones++;
                 int indice = i ;
                 for (int j = i+1; j < candidatos.length; j++) {
                     indice= j;
                 }    
-                // intercabio 
             if (indice != i) {
             Candidato temp = candidatos[i];
             candidatos[i] = candidatos[indice];
             candidatos[indice] = temp;
-            intercambios++;
+            this.intercambios++;
             }
-            long tiempoFin = System.nanoTime();
         }
+        long tiempoFin = System.nanoTime();
+        this.tiempo = tiempoFin-tiempoInicio;
         return candidatos;
     }
+    public int getIntercambios() {
+        return intercambios;
+    }
 
+    public void setIntercambios(int intercambios) {
+        this.intercambios = intercambios;
+    }
+
+    public int getComparaciones() {
+        return comparaciones;
+    }
+
+    public void setComparaciones(int comparaciones) {
+        this.comparaciones = comparaciones;
+    }
+
+    public long getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(long tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public Ordenamiento() {
+        this.intercambios = 0;
+        this.comparaciones = 0;
+        this.tiempo = 0;
+    }
 }
